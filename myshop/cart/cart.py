e423ee89dd26d23ccb5d796e79529fa96082bf4b
Armintavakol1376 <130907754+Armintavakol1376@users.noreply.github.com>
2023-08-11 09:38:02 +0430

    I debuged it

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -11,10 +11,31 @@ class Cart:
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-        # save an empty cart in the session
+            # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products
+        from the database.
+        """
+        product_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, override_quantity=False):
         """
@@ -22,8 +43,8 @@ class Cart:
         """
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
         if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -43,34 +64,10 @@ class Cart:
             del self.cart[product_id]
             self.save()
 
-    def __iter__(self):
-        """
-        Iterate over the items in the cart and get the products
-        from the database.
-        """
-        product_ids = self.cart.keys()
-        # get the product objects and add them to the cart
-        products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]['product'] = product
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-        item['total_price'] = item['price'] * item['quantity']
-        yield item
-
-    def __len__(self):
-        """
-        Chapter 8 355
-        Count all items in the cart.
-        """
-        return sum(item['quantity'] for item in self.cart.values())
-
-    def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.
-                   cart.values())
-
     def clear(self):
         # remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
